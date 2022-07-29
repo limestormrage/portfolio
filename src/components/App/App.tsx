72927c1a5b123445d8073cb4f1efd23d48ea9ad4
ваше имя <dimarik-tixonov@yandex.ru>
2022-07-30 02:13:04 +0300

    add: добавит фавиконку для приложения

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
           {' '}
