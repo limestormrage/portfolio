9fd15eeff3980540d844b0474b88a6cf3b993589
ваше имя <dimarik-tixonov@yandex.ru>
2022-08-07 22:33:11 +0300

    add: добавит функциональность мобильному меню

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import cn from 'classnames';
+import React, { useState } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { HeaderLink } from './const';
 import styles from './Header.module.scss';
 
 function Header(): JSX.Element {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
-    <header>
+    <header className={styles.header}>
       <div className="container">
-        <nav>
+        <button
+          type="button"
+          onClick={() => setOpenMenu(!openMenu)}
+          className={cn(styles.btnMenu, {
+            [styles.burger]: !openMenu,
+            [styles.btnClose]: openMenu,
+          })}
+        >
+          <span />
+        </button>
+        <nav className={cn(styles.nav, {
+          [styles.navOpen]: openMenu,
+        })}
+        >
           <ul className={styles.navList}>
             {Object.values(HeaderLink).map(({ title, link }) => (
               <li
+                className={styles.navItem}
                 key={title}
               >
                 <AnchorLink
